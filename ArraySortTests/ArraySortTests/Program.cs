c317cbd2c9ed36efdac03852010f675b3c6d2486
Evgeny <evolenko@inbox.ru>
2023-07-15 20:24:32 +0300

    Initial commit

---
+++ ArraySortTests/ArraySortTests/Program.cs
@@ -0,0 +1,16 @@
+﻿using BenchmarkDotNet.Running;
+
+namespace ArraySortTests
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            BenchmarkSwitcher
+                .FromAssembly(typeof(Program).Assembly)
+                .Run(args, new BenchmarkDotNet.Configs.DebugInProcessConfig());
+
+            BenchmarkRunner.Run<SortTests>();
+        }
+    }
+}
---
+++ ArraySortTests/ArraySortTests/SortTests.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArraySortTests
+{
+
+    [MemoryDiagnoser]
+    public class SortTests
+    {
+        static int[] array = ArrayUtils.prepareArray(50000);
+
+
+        [Benchmark]
+        public void DirectSortTest()
+        {
+            SortUtils.directSort((int[])array.Clone());
+        }
+
+        [Benchmark]
+        public void QuickSortTest()
+        {
+            SortUtils.quickSort((int[])array.Clone());
+        }
+
+    }
+}
